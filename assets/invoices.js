458fb739f7200f0e751d5d60f8cdd3d5b48743aa
eddymens <kwasiamantin@gmail.com>
2020-09-04 08:42:03 +0200

    init commit

---
+++ assets/invoices.js
@@ -0,0 +1,6 @@
+window.invoices = [
+    new Invoice(window.projects[0], 48, 4, window.months[12], 2018, "email@email.com", window.currencies[1],  window.paymentDetails[0])
+    .addItem("26/11/2018 Installation & Setup of required software on provided server", 61, 4)
+    .addItem("27/11/2018 - 30/11/2018 User Interface Development", 53, 16)
+    .done(),
+];
